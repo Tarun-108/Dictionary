0c563d279e3a65a655a27d2fd774748cdc6bf2b8
Tarun-108 <tarunstu125@gmail.com>
2022-07-08 03:24:51 +0530

    Dao created

--- app/src/main/java/com/taruns/dictionary/feature_dictonary/data/local/WordInfoDao.kt
+++ app/src/main/java/com/taruns/dictionary/feature_dictonary/data/local/WordInfoDao.kt
@@ -3,6 +3,7 @@ package com.taruns.dictionary.feature_dictonary.data.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.taruns.dictionary.feature_dictonary.data.local.entity.WordInfoEntity
 
 @Dao
@@ -10,6 +11,12 @@ interface WordInfoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertWordInfo(infos: List<WordInfoEntity>)
+
+    @Query("DELETE FROM wordinfoentity WHERE word IN(:words)")
+    suspend fun deleteWordInfos(words: List<String>)
+
+    @Query("SELECT * FROM wordinfoentity WHERE word LIKE '%' || :word || '%'")
+    suspend fun getWordInfo(word: String): List<WordInfoEntity>
 
 
 }
